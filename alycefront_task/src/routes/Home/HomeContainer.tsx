4437f4b3d934ee5252ed1f940d6f5efb565d5c16
YoungMin Lee <miniyoung37@gmail.com>
2020-10-24 19:45:58 +0900

    update login function

--- alycefront_task/src/routes/Home/HomeContainer.tsx
+++ alycefront_task/src/routes/Home/HomeContainer.tsx
@@ -1,12 +1,124 @@
-import React from "react";
+import * as React from "react";
 import HomePresenter from "./HomePresenter";
+import { message } from "antd";
 
-interface State {}
+interface Props {}
+interface State {
+  searchTerm: string;
+  userId: string;
+  userPassword: string;
+  modalVisible: boolean;
+  isUser: boolean;
+  loginUser: string;
+  [key: string]: string | any;
+}
 
-class HomeContainer extends React.Component<State> {
-  state: State = {};
+class HomeContainer extends React.Component<Props, State> {
+  state: State = {
+    searchTerm: "",
+    userId: "",
+    userPassword: "",
+    modalVisible: false,
+    isUser: false,
+    loginUser: "",
+  };
+
+  componentDidMount() {
+    const { isUser } = this.state;
+    localStorage.setItem("isUser", `${isUser}`);
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log("e :>> ", e.target.value);
+    console.log("e :>> ", e.target.name);
+    const name: string = e.target.name;
+    this.setState({
+      [name]: e.target.value,
+    });
+  };
+
+  handleShowModal = (): void => {
+    console.log("click :>> ");
+    this.setState({
+      modalVisible: true,
+    });
+  };
+
+  handleCancel = (): void => {
+    this.setState({
+      modalVisible: false,
+      userId: "",
+      userPassword: "",
+    });
+  };
+
+  handleSubmit = (): void => {
+    const { userId, userPassword, isUser } = this.state;
+    const adminId: string = "alyce";
+    const adminPassword: string = "alyce123";
+    if (adminId === userId && adminPassword === userPassword) {
+      message.success("로그인 성공");
+      localStorage.setItem("isUser", `${!isUser}`);
+      localStorage.setItem("loginUser", `${userId}`);
+      this.setState({
+        modalVisible: false,
+        userId: "",
+        userPassword: "",
+        isUser: true,
+        loginUser: userId,
+      });
+    } else {
+      message.error("회원이 아닙니다.");
+      this.setState({
+        modalVisible: false,
+        userId: "",
+        userPassword: "",
+      });
+    }
+  };
+
+  handleLogOut = (): void => {
+    const { isUser } = this.state;
+    message.success("로그아웃");
+    localStorage.setItem("loginUser", "");
+    localStorage.setItem("isUser", `${!isUser}`);
+    this.setState({
+      isUser: false,
+      loginUser: "",
+    });
+  };
   render() {
-    return <HomePresenter />;
+    const {
+      searchTerm,
+      modalVisible,
+      userId,
+      userPassword,
+      isUser,
+      loginUser,
+    } = this.state;
+    const {
+      handleChange,
+      handleShowModal,
+      handleCancel,
+      handleSubmit,
+      handleLogOut,
+    } = this;
+    console.log("this.state :>> ", this.state);
+    return (
+      <HomePresenter
+        userId={userId}
+        userPassword={userPassword}
+        searchTerm={searchTerm}
+        modalVisible={modalVisible}
+        isUser={isUser}
+        loginUser={loginUser}
+        handleChange={handleChange}
+        handleShowModal={() => handleShowModal()}
+        handleCancel={() => handleCancel()}
+        handleSubmit={() => handleSubmit()}
+        handleLogOut={() => handleLogOut()}
+      />
+    );
   }
 }
 
--- alycefront_task/src/routes/Home/HomePresenter.tsx
+++ alycefront_task/src/routes/Home/HomePresenter.tsx
@@ -1,10 +1,83 @@
-import React from "react";
+import * as React from "react";
+import LoginForm from "../../component/LoginForm";
+import styled from "styled-components";
+import { Layout, Button, Modal } from "antd";
 
-interface HomeProps {}
+const { Content } = Layout;
 
-const HomePresenter: React.FC<HomeProps> = () => (
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  font-size: 16px;
+  font-weight: bold;
+  align-items: center;
+  margin-right: 15px;
+`;
+
+interface HomeProps {
+  searchTerm: string;
+  userId: string;
+  userPassword: string;
+  modalVisible: boolean;
+  isUser: boolean;
+  loginUser: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleShowModal(): void;
+  handleCancel(): void;
+  handleSubmit(): void;
+  handleLogOut(): void;
+}
+
+const HomePresenter: React.FC<HomeProps> = ({
+  searchTerm,
+  userId,
+  userPassword,
+  modalVisible,
+  handleChange,
+  handleShowModal,
+  handleCancel,
+  handleSubmit,
+  isUser,
+  loginUser,
+  handleLogOut,
+}) => (
   <>
-    <div>Home</div>
+    <Content style={{ margin: "0 auto", width: 850 }}>
+      {!isUser ? (
+        <ButtonContainer>
+          <Button type="primary" name="login" onClick={handleShowModal}>
+            로그인
+          </Button>
+        </ButtonContainer>
+      ) : (
+        <ButtonContainer>
+          <Title>{`${loginUser} 님`}</Title>
+          <Button type="primary" name="logout" onClick={handleLogOut}>
+            로그아웃
+          </Button>
+        </ButtonContainer>
+      )}
+    </Content>
+
+    <Modal
+      visible={modalVisible}
+      cancelText="취소"
+      okText={"로그인"}
+      onOk={handleSubmit}
+      onCancel={handleCancel}
+    >
+      <LoginForm
+        handleChange={handleChange}
+        userId={userId}
+        userPassword={userPassword}
+      />
+    </Modal>
   </>
 );
 
