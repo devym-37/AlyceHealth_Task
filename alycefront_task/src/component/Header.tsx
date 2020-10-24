0978db57b9fb250da23afa08bee7a7159f03a504
YoungMin Lee <miniyoung37@gmail.com>
2020-10-24 15:37:52 +0900

    add Header Style & GlobalStyles

---
+++ alycefront_task/src/component/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+import { MailOutlined, AppstoreOutlined } from "@ant-design/icons";
+
+interface State {}
+
+class Headers extends React.Component<State> {
+  state: State = {};
+
+  //   handleClick = (e: any) => {
+  //     console.log("click ", e);
+  //     this.setState({ current: e.key });
+  //   };
+
+  render() {
+    return (
+      <Menu mode="horizontal">
+        <Menu.Item key="mail" icon={<AppstoreOutlined />}>
+          <Link to={"/"}>홈</Link>
+        </Menu.Item>
+        <Menu.Item key="app" icon={<MailOutlined />}>
+          <Link to={"/likeNews"}>즐겨찾기</Link>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default Headers;
--- alycefront_task/src/component/Router.tsx
+++ alycefront_task/src/component/Router.tsx
@@ -5,18 +5,26 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
+import { Layout } from "antd";
+
+import Header from "./Header";
 
 import HomeScreen from "../routes/Home";
 import DetailScreen from "../routes/Detail";
+import LikeScreen from "../routes/Like";
 
 export default () => (
   <Router>
     <>
-      <Switch>
-        <Route path="/" exact component={HomeScreen} />
-        <Route path="/detail" component={DetailScreen} />
-        <Redirect from="*" to="/" />
-      </Switch>
+      <Layout className="layout" style={{ minHeight: "100vh" }}>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/detail" component={DetailScreen} />
+          <Route path="/likeNews" component={LikeScreen} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Layout>
     </>
   </Router>
 );
