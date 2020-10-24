522ff22de6be625c7631f38c2c7b05142f18c745
YoungMin Lee <miniyoung37@gmail.com>
2020-10-25 00:22:57 +0900

    add App components GlobalStyle Components

--- alycefront_task/src/component/App.tsx
+++ alycefront_task/src/component/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import * as React from "react";
 import Router from "./Router";
+import GlobalStyle from "./GlobalStyles";
 
 function App() {
   return (
     <>
       <Router />
+      <GlobalStyle />
     </>
   );
 }
